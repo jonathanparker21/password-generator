ccff97c988890c19496d7b71e48f5a133392bc94
Jonathan Parker <jprkr21@gmail.com>
2021-11-25 13:02:53 -0600

    fixed undefined at beginning of passwordChar string

--- script.js
+++ script.js
@@ -18,75 +18,83 @@ THEN the password is either displayed in an alert or written to the page
 */
 
 
+var lowerCaseOptions = 'qwertyuiopasdfghjklzxcvbnm';
+var upperCaseOptions = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+var numberOptions = '1234567890';
+var specialOptions = '!@#$%^&*()_+",.-/?:;<=>[]{}`|~';
 
 
 function generatePassword() {
   // 1. prompt the user for the password criteria
   //    a. password length from 8 to 128 characters
-  var charLength = +prompt('How many characters would you like your password to contain? Choose a number from 8 to 128.'); // add a + in front of prompt to turn it from a string to a number?
+  var charLength = Number(prompt('How many characters would you like your password to contain? Choose a number from 8 to 128.'));
+  
   //    b. confirm user inputs a number from 8 to 128
   if (!charLength) {
     alert('Please enter a password length.');
-    charLength = prompt('How many characters would you like your password to contain? Choose a number from 8 to 128.');
-  } else if (charLength < 8 || charLength > 128) {
-    alert('You must enter a number from 8 to 128.');
-    charLength = prompt('How many characters would you like your password to contain? Choose a number from 8 to 128.');
+    return charLength = Number(prompt('How many characters would you like your password to contain? Choose a number from 8 to 128.'));
   }
-  //    c. lowercase, uppercase, numbers, special characters
-  var hasLowercase = confirm('Click OK to confirm including lowercase characters.');
-  var hasUppercase = confirm('Click OK to confirm including uppercase characters.');
-  var hasNumber = confirm('Click OK to confirm including numeric characters.');
-  var hasSpecial = confirm('Click OK to confirm including special characters.');
   
-  console.log(charLength, hasLowercase, hasUppercase, hasNumber, hasSpecial);
+  // this only works is the page is reefreshed - if user ree-enters a number, it appends # to page
+  if (charLength < 8 || charLength > 128) {
+    alert('You must enter a number from 8 to 128.');
+    return charLength = Number(prompt('How many characters would you like your password to contain? Choose a number from 8 to 128.'));
+  } else {
+    //    c. lowercase, uppercase, numbers, special characters
+    var hasLowercase = confirm('Click OK to confirm including lowercase characters.');
+    var hasUppercase = confirm('Click OK to confirm including uppercase characters.');
+    var hasNumber = confirm('Click OK to confirm including numeric characters.');
+    var hasSpecial = confirm('Click OK to confirm including special characters.');
+  }
   
   // 2. validate the input
   //    a. confirm user selected at least one character type
-  var charTypes = hasLowercase + hasUppercase + hasNumber + hasSpecial;
-  if (charTypes === 0) {
-      return '';
-    }
-  //    b. filter out cancels
+  if (!hasLowercase && !hasUppercase && !hasNumber && !hasSpecial) {
+    alert('You must select at least one character type.')
+    hasLowercase = confirm('Click OK to confirm including lowercase characters.');
+    hasUppercase = confirm('Click OK to confirm including uppercase characters.');
+    hasNumber = confirm('Click OK to confirm including numeric characters.');
+    hasSpecial = confirm('Click OK to confirm including special characters.');
+  }
+  
+  // but will this have app make sure there's at least one of each character selected (or is that beyond the scope of this assignment?)
+  var passwordChar = '';
+  
+  if (hasLowercase) {
+    passwordChar += lowerCaseOptions;
+  }
+  
+  if (hasUppercase) {
+    passwordChar += upperCaseOptions;
+  }
+  
+  if (hasNumber) {
+    passwordChar += numberOptions;
+  }
+  
+  if (hasSpecial) {
+    passwordChar += specialOptions;
+  }
+  
+  console.log(passwordChar);
+
+  //    b. filter out cancels - is this being done automatically above?
+  
   // 3. generate password based on criteria
   //    a. initialize a password variable string to continuously build on
+  var generatedPassword = '';
+
   //    b. loop over charLength & call generator function for each type
+  for (var i = 0; i < charLength; i++) {
+    generatedPassword += passwordChar[Math.floor(Math.random() * passwordChar.length)];
+  }
+
+  console.log(generatedPassword);
+  
+  return generatedPassword;
+
   // 4. display password to the page
-    
-  // var generatedPassword = '';
-
-  // var typesArr = [{hasLowercase}, {hasUppercase}, {hasNumber}, {hasSpecial}].filter(item => Object.values(item) [0]);
-
-  // for (let i = 8; i < 129; i += typesCount) {
-    //   typesArr.forEach(type => {
-      //     var funcName = Object.keys(type)[0];
-      //     // console.log('funcName: ', funcName);
-      
-      //     generatedPassword += randomFunc[funcName]();
-      //   });
-      // }
-
-  return generatedPassword; 
 }
-
-
-
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-function getRandomSpecial() {
-  var specials = '~`!@#$%^&*()_-+=:;';
-  return specials[Math.floor(Math.random() * specials.length)];
-}
-      
-      
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
